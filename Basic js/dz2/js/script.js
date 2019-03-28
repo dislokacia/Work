beca8652d72f0329b24b14c6225999912d15601d
dislokacia <anprudnikov92@gmail.com>
2019-03-28 14:46:39 +0200

    dz2 added the same in if/else

--- Basic js/dz2/js/script.js
+++ Basic js/dz2/js/script.js
@@ -7,28 +7,44 @@ var login;
  */
 var password;
 // asking the login
-var login = prompt( 'Введите логин' );
+login = prompt( 'Введите логин' );
 
-// begin switch/case
-switch (login) {            
-    case 'Админ':           //if login = 'Админ'
-    var password = prompt( 'Введите пароль' );      // then asking for password
-    switch (password) {     // begin new switch/case
-        case 'Черный Властелин':    //if password = 'Черный Властелин'
-        alert( 'С возвращением в Мордор!' );    // writing 'С возвращением в Мордор!'
-        break;  // if all right - breaking the app
-        case null:  //if password was cancelled
-        alert( 'Вход отменен' );    // writing 'Вход отменен'
-        break;  // if all right - breaking the app
-        default:    // if password = something else
-        alert( 'Ха-ха! Пароль неверен! Шах и мат, досвидули!' );  // writing 'Ха-ха! Пароль неверен! Шах и мат, досвидули!'
-        break;  // if all right - breaking the app
+// // begin switch/case
+// switch (login) {            
+//     case 'Админ':           //if login = 'Админ'
+//     password = prompt( 'Введите пароль' );      // then asking for password
+//     switch (password) {     // begin new switch/case
+//         case 'Черный Властелин':    //if password = 'Черный Властелин'
+//         alert( 'С возвращением в Мордор!' );    // writing 'С возвращением в Мордор!'
+//         break;  // if all right - breaking the app
+//         case null:  //if password was cancelled
+//         alert( 'Вход отменен' );    // writing 'Вход отменен'
+//         break;  // if all right - breaking the app
+//         default:    // if password = something else
+//         alert( 'Ха-ха! Пароль неверен! Шах и мат, досвидули!' );  // writing 'Ха-ха! Пароль неверен! Шах и мат, досвидули!'
+//         break;  // if all right - breaking the app
+//     }
+//     break;  // if all right - breaking the app
+//     case null:  //if login was cancelled
+//     alert( 'Вход отменен' );    // writing 'Вход отменен'
+//     break;  // if all right - breaking the app
+//     default:    // if login = something else
+//     alert( 'Я вас не знаю, уходите' );  //then writing 'Я вас не знаю, уходите'
+//     break;  // if all right - breaking the app
+// }
+
+
+if ( login == 'Админ' ) {
+    password = prompt( 'Введите пароль' );
+    if ( password == 'Черный Властелин' ) {
+        alert( 'С возвращением в Мордор!' );
+    } else if ( password == null ) {
+        alert( 'Вход отменен' );
+    } else {
+        alert( 'Ха-ха! Пароль неверен! Шах и мат, досвидули!' );
     }
-    break;  // if all right - breaking the app
-    case null:  //if login was cancelled
-    alert( 'Вход отменен' );    // writing 'Вход отменен'
-    break;  // if all right - breaking the app
-    default:    // if login = something else
-    alert( 'Я вас не знаю, уходите' );  //then writing 'Я вас не знаю, уходите'
-    break;  // if all right - breaking the app
+} else if ( login == null) {
+    alert( 'Вход отменен' );
+} else {
+    alert( 'Я вас не знаю, уходите' );
 }
