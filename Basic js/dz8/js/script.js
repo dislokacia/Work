75e0c8d8a56bf77dd9682d1807896021f8a263d9
dislokacia <anprudnikov92@gmail.com>
2019-04-22 18:36:36 +0300

    dz8 added tabs

---
+++ Basic js/dz8/js/script.js
@@ -0,0 +1,13 @@
+function sport(event, sport) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(sport).style.display = "block";
+  event.currentTarget.className += " active";
+}
